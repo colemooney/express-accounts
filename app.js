4f1730f463ce7aa784ba4bdc77126f13a16e3505
Cole <Cole@iMac.localdomain>
2020-01-31 21:17:22 -0500

    signup page

--- app.js
+++ app.js
@@ -10,14 +10,22 @@ const logger       = require('morgan');
 const path         = require('path');
 
 
+// app.js
+// Mongoose configuration
+mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/express-accounts', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
+  .connect('mongodb://localhost/express-accounts', {useMongoClient: true})
+  .then(() => {
+    console.log('Connected to Mongo!')
+  }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
+// ...other code
+
+// Routes
+const authRoutes = require("./routes/auth-routes");
+app.use('/', authRoutes);
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
